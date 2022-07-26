51e6df1b7aed912fba05b62e51efcf48a3461b62
AgestiaD <110050372+AgestiaD@users.noreply.github.com>
2022-07-26 23:10:12 +0700

    Add files via upload

---
+++ Full Finished frontend (hasil yang saya dapat)/web.js
@@ -0,0 +1,16 @@
+function replace(){
+    document.getElementById("detail1").style.display="none";
+    document.getElementById("detail2").style.display="block";
+    document.getElementById("button1").style.display="none";
+    document.getElementById("button2").style.display="block";
+}
+
+function replaceBack(){
+    document.getElementById("detail2").style.display="none";
+    document.getElementById("detail1").style.display="block";
+    document.getElementById("button2").style.display="none";
+    document.getElementById("button1").style.display="block";
+}
+
+
+
